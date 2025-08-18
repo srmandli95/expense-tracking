79223e2b06af383041706a2d33b84c8e111e6755
smandli <smandli@ford.com>
2025-08-18 17:37:53 -0400

    updated the welcome page

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Me from "./pages/Me";
 import Expenses from "./pages/Expenses";
+import Welcome from "./pages/Welcome";
 
-export default function App() {
+function AppContent() {
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
-      <Navbar />
+    <>
+      {/* Conditionally render Navbar */}
+      {location.pathname !== "/" && <Navbar />}
       <div className="p-4">
         <Routes>
+          <Route path="/" element={<Welcome />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/me" element={<ProtectedRoute><Me /></ProtectedRoute>} />
@@ -19,6 +24,14 @@ export default function App() {
           <Route path="*" element={<Login />} />
         </Routes>
       </div>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   );
 }
